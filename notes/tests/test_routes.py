d211e58d24dd2cc170a943b54fbba31914e8162e
Bogatyrev Sergey <bogatyrev.sergeyxxx@yandex.ru>
2025-07-04 15:02:02 +0500

    edit delete

--- notes/tests/test_routes.py
+++ notes/tests/test_routes.py
@@ -14,6 +14,7 @@ class TestRoutes(TestCase):
     @classmethod
     def setUpTestData(cls):
         cls.author = User.objects.create(username='Автор')
+        cls.reader = User.objects.create(username='Читатель')
         cls.notes = Note.objects.create(title='Заголовок', text='Текст', slug='test', author=cls.author)
 
     def test_home_page(self):
@@ -23,8 +24,16 @@ class TestRoutes(TestCase):
 
 
 
-    def test_detail_page(self):
-        self.client.force_login(self.author)
-        url = reverse('notes:detail', args=[self.notes.slug])
-        response = self.client.get(url)
-        self.assertEqual(response.status_code, HTTPStatus.OK) 
+    def test_detail_edit_delete_note(self):
+        users_statuses = (
+            (self.author, HTTPStatus.OK),
+            (self.reader, HTTPStatus.NOT_FOUND),
+        )
+        for user, status in users_statuses:
+            self.client.force_login(user)
+            for name in ('notes:edit', 'notes:detail', 'notes:delete'):
+                with self.subTest(user=user, name=name):
+                    url = reverse('notes:detail', args=[self.notes.slug])
+                    response = self.client.get(url)
+                    self.assertEqual(response.status_code, status)
+
