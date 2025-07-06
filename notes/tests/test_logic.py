5ee2499bfb4726bee30e23f5cf39f2557a875f1b
Bogatyrev Sergey <bogatyrev.sergeyxxx@yandex.ru>
2025-07-06 16:43:15 +0500

    slug is none

--- notes/tests/test_logic.py
+++ notes/tests/test_logic.py
@@ -1,6 +1,6 @@
 from http import HTTPStatus
-
-from django.test import TestCase
+from pytils.translit import slugify
+from django.test import TestCase, Client
 from django.urls import reverse
 from notes.models import Note
 from django.contrib.auth import get_user_model
@@ -13,19 +13,37 @@ User = get_user_model()
 class TestLogic(TestCase):
 
     ADD_URL = reverse('notes:add')
+    SUCCESS_URL = reverse('notes:success')
     # EDIT_URL = reverse('notes:edit', [self.notes.slug])
     # DELETE_URL = reverse('notes:delete', [self.notes.slug])
 
     @classmethod
     def setUpTestData(cls):
-        cls.author = User.objects.create(username='Автор')
+        cls.user = User.objects.create(username='Автор')
+        cls.author = Client()
+        cls.author.force_login(cls.user)
         cls.reader = User.objects.create(username='Читатель')
+        cls.anonimus = Client()
         # cls.notes = Note.objects.create(title='Заголовок', text='Текст', slug='test', author=cls.author)
         cls.form_data = {'title': 'Заголовок', 'text': 'Текст', 'slug':'test'}
+        cls.form_data_no_slug = {'title': 'Заголовок', 'text': 'Текст', 'slug': None, 'author': cls.author}
 
+
+    def test_slug_is_none(self):
+        response = self.client.post(self.ADD_URL, data=self.form_data)
+        login_url = reverse('users:login')
+        redirect_url = f'{login_url}?next={self.ADD_URL}'
+        self.assertRedirects(response, redirect_url)
+        if self.form_data_no_slug['slug'] is None:
+            slug = slugify(self.form_data_no_slug['title'])
+        self.author.force_login(self.user)
+        form_data_no_slug = {'title': 'Заголовок', 'text': 'Текст', 'slug': slug, 'author': self.author}
+        response = self.author.post(self.ADD_URL, data=form_data_no_slug)
+        notes_count = Note.objects.count()
+        self.assertEqual(notes_count, 1)
 
     def test_add_note_user(self):
-        self.client.force_login(self.author)
+        self.client.force_login(self.user)
         response = self.client.post(self.ADD_URL, data=self.form_data)
         self.assertEqual(response.status_code, HTTPStatus.FOUND)
         notes_count = Note.objects.count()
@@ -41,16 +59,13 @@ class TestLogic(TestCase):
 
     def test_slug_is_unique(self):
         all_notes = [
-            Note(title='Заголовок', text='Текст', slug=f'test {index}', author=self.author)
+            Note(title='Заголовок', text='Текст', slug=f'test {index}', author=self.user)
             for index in range(2)
         ]
         Note.objects.bulk_create(all_notes)
-        self.client.force_login(self.author)
+        self.author.force_login(self.user)
         response_1 = self.client.post(self.ADD_URL, data=self.form_data)
         response_2 = self.client.post(self.ADD_URL, data=self.form_data)
         slug_1 = Note.objects.first().slug
         slug_2 = Note.objects.last().slug
         self.assertNotEqual(slug_1, slug_2)
-
-
-
