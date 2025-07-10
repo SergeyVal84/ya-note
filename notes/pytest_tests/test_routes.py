449d00a75869b2ae037bb206b4ec039efdea6c10
Sergey Bogatyrev <bogatyrev.sergeyxxx@yandex.ru>
2025-07-10 14:15:51 +0500

    параметрим фикстуры

--- notes/pytest_tests/test_routes.py
+++ notes/pytest_tests/test_routes.py
@@ -2,7 +2,7 @@ import pytest
 from http import HTTPStatus
 
 from django.urls import reverse
-
+from pytest_lazyfixture import lazy_fixture
 
 @pytest.mark.parametrize(
     ('name', 'method'),
@@ -22,3 +22,31 @@ def test_pages_availability_for_auth_user(not_author_client, name):
     url = reverse(name)
     response = not_author_client.get(url)
     assert response.status_code == HTTPStatus.OK
+
+
+@pytest.mark.parametrize(
+    'name',
+    ('notes:detail', 'notes:edit', 'notes:delete'),
+)
+def test_pages_availability_for_author(author_client, name, note):
+    url = reverse(name, args=(note.slug,))
+    response = author_client.get(url)
+    assert response.status_code == HTTPStatus.OK
+
+@pytest.mark.parametrize(
+    'parametrized_client, expected_status',
+    (
+        (lazy_fixture('not_author_client'), HTTPStatus.NOT_FOUND),
+        (lazy_fixture('author_client'), HTTPStatus.OK)
+    ),
+)
+@pytest.mark.parametrize(
+    'name',
+    ('notes:detail', 'notes:edit', 'notes:delete'),
+)
+def test_pages_availability_for_different_users(
+        parametrized_client, name, note, expected_status
+):
+    url = reverse(name, args=(note.slug,))
+    response = parametrized_client.get(url)
+    assert response.status_code == expected_status
